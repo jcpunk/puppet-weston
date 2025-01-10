099c8f10ac7248f980cd12968f96ef03761ce1aa
Pat Riehecky <riehecky@fnal.gov>
2025-01-10 12:11:31 -0600

    Add export to novnc class

---
+++ manifests/vnc_server/export.pp
@@ -0,0 +1,25 @@
+# @summary Map defined VNC servers into NOVNC client
+#
+# @param vnc_server_hostname
+#   Hostname to use as the default server target
+# @param vnc_servers
+#   Hash of vnc_servers to export.
+#   You probably should just let inheritance do the work here
+class weston::vnc_server::export (
+  String $vnc_server_hostname = 'localhost',
+  Hash $vnc_sessions = $weston::vnc_server::vnc_sessions,
+) inherits weston::vnc_server {
+  $connections = $vnc_sessions.reduce({}) |$memo, $user_info| {
+    $displaynumber = $user_info[1]['displaynumber']
+    if $displaynumber < 5900 {
+      $real_displaynumber = $displaynumber + 5900
+    } else {
+      $real_displaynumber = $displaynumber
+    }
+    $memo + { $user_info[0] => "${vnc_server_hostname}:${real_displaynumber}" }
+  }
+
+  class { 'novnc':
+    vnc_servers => $connections,
+  }
+}
---
+++ spec/classes/vnc_server/export_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'weston::vnc_server::export' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      context 'when using defaults' do
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_class('novnc') }
+      end
+
+      context 'with interesting arguments' do
+        let(:params) do
+          {
+            'vnc_sessions' => {
+              'userA' => {
+                'comment' => 'a comment',
+                'displaynumber' => 1,
+                'user_can_manage' => true,
+              },
+              'userB' => {
+                'comment' => 'a different comment',
+                'displaynumber' => 12,
+                'user_can_manage' => false,
+              },
+            }
+          }
+        end
+
+        it { is_expected.to compile }
+        it { is_expected.to contain_class('novnc') }
+        it {
+          is_expected.to contain_file('/etc/websockify/tokens.cfg')
+            .with_ensure('file')
+            .with_owner('root')
+            .with_group('novnc')
+            .with_mode('0640')
+            .with_content(%r{2b8ccffa: localhost:5901})
+            .with_content(%r{82561bf3: localhost:5912})
+        }
+        it {
+          is_expected.to contain_file('/var/www/novnc_users_list.html')
+        }
+      end
+    end
+  end
+end
